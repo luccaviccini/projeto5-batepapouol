04c5b6b82a53716efa2609a6fe65f02de9555e58
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2022-10-29 12:47:09 -0300

    basic functions of add user, get users and get messages ok

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-// global variables
-let username = null;   // name of the user
+// ----*----*----*----*----START of global variables ----*----*----*----*----*----* \\
+// get messages
+const urlMessages = "https://mock-api.driven.com.br/api/v6/uol/messages";
+// checks if user is online
+const urlStatus = "https://mock-api.driven.com.br/api/v6/uol/status";
+// sends name to api
+const urlName = "https://mock-api.driven.com.br/api/v6/uol/participants";
+// query selector to key elements
+let currentUser = null;
 
-// function that gets the namoe from input text on class login
+// ----*----*----*----*----END of global variables ----*----*----*----*----*----* \\
 
-function getUserName() {
-    username = document.querySelector('.login input').value;    
-    console.log(username);
-    document.querySelector('.login').style.display = 'none';
-    document.querySelector('.body-container').style.display = 'block';
+// function that is called when "Entrar" button is pressed
+function btLogin() {
+  currentUser = document.querySelector(".login input").value;
+  addUser(currentUser);
+}
+
+function addUser(currentUser) {
+  const promisse = axios({
+    method: "POST",
+    url: urlName,
+    data: { name: currentUser },
+  });
+  promisse.then((response) => {
+    console.log(currentUser);
+    console.log(response.data);
+    console.log("--------- addUser -----------");
+    getUsers();
+    getMessages();
+    document.querySelector(".login").style.display = "none";
+    document.querySelector(".body-container").style.display = "block";
+  });
+  promisse.catch((error) => {
+    //console.log("Status code: " + error.response.status);
+  });
+}
+
+function getUsers() {
+  const promisse = axios({ method: "GET", url: urlName });
+  promisse.then((response) => {
+    console.log(response.data);
+    console.log("--------- getUsers -----------");
+  });
+  promisse.catch((error) => {
+    console.log(error);
+  });
+}
+
+function getMessages() {
+  const promisse = axios({ method: "GET", url: urlMessages });
+  promisse.then((response) => {
+    console.log(response.data[0]);
+    console.log("--------- getMessages -----------");
+  });
+  promisse.catch((error) => {
+    console.log(error);
+  });
 }
